b770636db55d13a415e1c76d6e0f26e70ecceaa3
git config --global user.namNoorFares <s12028537@stu.najah.edu>
2022-05-07 01:20:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,3 +4,6 @@ const app = express();
 const port =process.env.PORT||3000;
 const mysql=require('mysql');
 const dotenv = require('dotenv');
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+dotenv.config({path: './.env'});
