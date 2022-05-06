d223af778d52c45d57760956805235f44b8e85f2
KhalidKhffaush <92662422+Khalidfathi@users.noreply.github.com>
2022-05-06 15:26:09 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,3 +7,25 @@ const dotenv = require('dotenv');
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 dotenv.config({path: './.env'});
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database:'bloodbank'
+  });
+  db.connect(error => {
+    if(error) {
+      console.log(error);
+    }else{
+      console.log('MYSQL connected...');
+    }
+  });
+  //creat Template using hbs
+  app.use(express.static('public'))
+  const handlebars = exphbs.create({ extname: '.hbs',});
+  app.engine('.hbs', handlebars.engine);
+  app.set('view engine', '.hbs');
+  app.listen(port, 
+    () => console.log(`Listening on port ${port}`));
+
+  
