3990c5d10756d744c51bb6c8cc69558a304bdf2d
KhalidKhffaush <92662422+Khalidfathi@users.noreply.github.com>
2022-05-09 09:46:57 -0700

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -16,3 +16,17 @@ connection.connect(error => {
       console.log('MYSQL connected of table user..*!');
     }
   });
+  //View Users
+exports.view = (req, res) => {
+    // User the connection
+    connection.query('SELECT * FROM user WHERE status = "active"', (err, rows) => {
+      // When done with the connection, release it
+      if (!err) {
+        let removedUser = req.query.removed;
+        res.render('home.hbs', { rows, removedUser });
+      } else {
+        console.log(err);
+      }
+      console.log('The data from user table: \n', rows);
+    });
+  }
