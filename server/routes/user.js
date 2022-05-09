b3ccf170911e2d90fbea7a770c98a9ac804b22c5
git config --global user.namNoorFares <s12028537@stu.najah.edu>
2022-05-09 19:58:22 +0300

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 // Routes
 router.get('/', userController.view);
+router.post('/', userController.find);
+
 module.exports = router;
