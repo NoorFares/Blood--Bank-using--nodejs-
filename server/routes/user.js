5c992cc2f408a4971ec22c8460e7fb31bded6483
KhalidKhffaush <92662422+Khalidfathi@users.noreply.github.com>
2022-05-09 09:47:53 -0700

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+// Routes
+router.get('/', userController.view);
 module.exports = router;
