d1c44ab2ba0cfc2afc4dec989db55af89471d027
git config --global user.namNoorFares <s12028537@stu.najah.edu>
2022-05-09 19:57:30 +0300

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -16,14 +16,27 @@ connection.connect(error => {
       console.log('MYSQL connected of table user..*!');
     }
   });
-  //View Users
+  //View Donrs
 exports.view = (req, res) => {
-    // User the connection
+    // Donrs  the connection
     connection.query('SELECT * FROM user WHERE status = "active"', (err, rows) => {
       // When done with the connection, release it
       if (!err) {
         let removedUser = req.query.removed;
         res.render('home.hbs', { rows, removedUser });
+      } else {
+        console.log(err);
+      }
+      console.log('The data from user table: \n', rows);
+    });
+  }
+  // Find User by Search
+  exports.find = (req, res) => {
+    let searchTerm = req.body.search;
+    // User the connection
+    connection.query('SELECT * FROM user WHERE  blood_type LIKE ?', [ searchTerm ], (err, rows) => {
+      if (!err) {
+        res.render('home.hbs', { rows });
       } else {
         console.log(err);
       }
