949ec5b08382853e454a733199dcfb2ce39e5ef8
KhalidKhffaush <92662422+Khalidfathi@users.noreply.github.com>
2022-05-13 14:12:25 -0700

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,5 +5,6 @@ const userController = require('../controllers/userController');
 router.get('/', userController.view);
 router.post('/', userController.find);
 router.get('/adduser', userController.form);
-
+router.post('/adduser', userController.create);
 module.exports = router;
+
